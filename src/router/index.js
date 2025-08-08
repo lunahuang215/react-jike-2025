a391437cac2b5f13a8c7a3adc1d70094c80e6871
Chace Shi <shichushi@gmail.com>
2025-08-07 18:10:49 -0700

    lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,14 @@ import { createBrowserRouter } from 'react-router-dom';
 import Login from '@/pages/Login';
 import Layout from '@/pages/Layout';
 import AuthRoute from '@/components/AuthRoute';
-import Home from '@/pages/Home';
-import Article from '@/pages/Article';
-import Publish from '@/pages/Publish';
+//import Home from '@/pages/Home';
+//import Article from '@/pages/Article';
+//import Publish from '@/pages/Publish';
+import {lazy, Suspense} from 'react';
+
+const Home = lazy(() => import('@/pages/Home'));
+const Article = lazy(() => import('@/pages/Article'));
+const Publish = lazy(() => import('@/pages/Publish'));
 
 const router = createBrowserRouter([
   {
@@ -13,15 +18,21 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <AuthRoute><Home /></AuthRoute>,
+        element: <Suspense fallback={<div>Loading...</div>}>
+          <Home />
+        </Suspense>,
       },
       {
         path: '/article',
-        element: <AuthRoute><Article /></AuthRoute>,
+        element: <Suspense fallback={<div>Loading...</div>}>
+          <Article />
+        </Suspense>,
       },
       {
         path: '/publish',
-        element: <AuthRoute><Publish /></AuthRoute>,
+        element: <Suspense fallback={<div>Loading...</div>}>
+          <Publish />
+        </Suspense>,
       },
     ],
   },
